c1b07e8251f05faac32d1f323dfc884559a866e0
Ting-Chun Yeh <85913042+tingchuny@users.noreply.github.com>
2021-06-30 17:29:33 -0500

    Update JOINs.sql

--- SQL_query_practice/Learn_SQL.com/JOINs.sql
+++ SQL_query_practice/Learn_SQL.com/JOINs.sql
@@ -1,31 +1,30 @@
 SELECT
-	s.date,
+    s.date,
     p.name,
     s.amount,
     n.calories
 FROM
-	sales_history 		AS s
-    JOIN product 		AS p ON s.product_id = p.id
+    sales_history         AS s
+    JOIN product         AS p ON s.product_id = p.id
     JOIN nutrition_data AS n ON n.product_id = p.id
 WHERE
-	p.id IN (13, 18, 15)
+    p.id IN (13, 18, 15)
 ORDER BY
-	s.date, n.calories;
-	
+    s.date, n.calories;
+    
 /*
 query items that is null between the 2 dates.
 */
 SELECT
-	p.name  AS product_name,
-    p.price	AS price,
+    p.name  AS product_name,
+    p.price    AS price,
     pr.name AS company_name
     
 FROM
-	product AS p
+    product AS p
     LEFT JOIN sales_history AS s 
-    	ON s.product_id = p.id
-    	AND s.date BETWEEN '2015-02-01' AND '2015-02-05'
+        ON s.product_id = p.id
+        AND s.date BETWEEN '2015-02-01' AND '2015-02-05'
     LEFT JOIN producer AS pr ON p.producer_id = pr.id
 WHERE
-	s.product_id IS NULL;
-
+    s.product_id IS NULL;
