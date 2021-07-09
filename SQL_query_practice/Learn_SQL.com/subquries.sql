6e7a0c3fb041c45a892aa813c2e4742bdea53dae
Ting-Chun Yeh <85913042+tingchuny@users.noreply.github.com>
2021-07-09 11:12:31 -0500

    Update subquries.sql

--- SQL_query_practice/Learn_SQL.com/subquries.sql
+++ SQL_query_practice/Learn_SQL.com/subquries.sql
@@ -16,3 +16,19 @@ HAVING
                    FROM     (SELECT COUNT (*) AS count1 FROM members GROUP BY orchestra_id)
                             AS d)
     
+
+
+/*
+correlated subquries
+*/
+SELECT
+	m.name AS member,
+    o.name AS orchestra
+FROM
+	members AS m
+    JOIN orchestras AS o ON m.orchestra_id = o.id
+WHERE
+	m.experience = 
+    (SELECT MAX(experience)
+    FROM members AS m1 
+    WHERE m1.orchestra_id = m.orchestra_id)
